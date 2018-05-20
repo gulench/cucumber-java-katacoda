97f5d1ec90103347efc4c6a2a871f7dc47e12d33
Gulen Chongtham <gulen.chongtham@tcs.com>
2018-05-20 01:52:02 +0000

    hands on

---
+++ src/test/java/com/play/bdd/steps/PlaySteps.java
@@ -0,0 +1,41 @@
+package com.play.bdd.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class PlaySteps {
+    @Given("^Cucumber is really easy to learn$")
+    public void cucumber_is_really_easy_to_learn() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("^I have to pass this hand on$")
+    public void i_have_to_pass_this_hand_on() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("^This will test my cucumber skills$")
+    public void this_will_test_my_cucumber_skills() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Given("^Play a learning platform$")
+    public void i_have_a_play() throws Throwable {
+        //System.out.println("This will print about play");
+        
+    }
+
+    @When("^I want to learn$")
+    public void love_for_play() throws Throwable {
+        //System.out.println("This will print love for play");
+    }
+
+    @Then("^I open play to learn and earn$")
+    public void benefits_of_play() throws Throwable {
+        //System.out.println("This will print benefits of play");
+    }
+}
---
+++ src/test/resources/cucumber/play.feature
@@ -0,0 +1,9 @@
+Feature: First cucumber HandsOn
+  This is my first cucumber hands on
+  Badly I want to crack it
+
+  Scenario: About my learning
+    Given Cucumber is really easy to learn
+    When I have to pass this hand on
+    Then This will test my cucumber skills
+
